fc1adafdc256b0a29f79d655a02ded2234990eb9
MlcnYlmz <odmelihcan@gmail.com>
2022-11-29 15:10:11 +0300

    Level Manager Update 2

--- Assets/Scripts/Data/UnityObjects/CD_Level.cs
+++ Assets/Scripts/Data/UnityObjects/CD_Level.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace Data.UnityObjects
 {
-    [CreateAssetMenu(fileName = "CD_Level", menuName = "Picker3D/CD_Level")]
+    [CreateAssetMenu(fileName = "CD_Level", menuName = "Picker3DGameEngines2Project/CD_Level")]
     public class CD_Level : ScriptableObject
     {
         public List<LevelData> LevelList = new List<LevelData>();
